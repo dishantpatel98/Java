d4b2e13203242e40409600bb67e88fa2f797db13
Dishant Janakbhai Patel <39313255+dishantpatel98@users.noreply.github.com>
2019-11-12 20:42:50 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,23 @@
+package employee;
+
+public class Employee {
+int id;
+String name;
+double totalSalary;
+
+public Employee(int id,String name) {
+	this.id=id;
+	this.name=name;
+	
+	
+}	
+
+public void calculateSalary() {
+	System.out.println("This is the function of base class");
+	
+
+	
+	
+}
+
+}
